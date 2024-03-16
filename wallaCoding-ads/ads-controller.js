7c9b18a0aa865088663636f56426a0116d41b7b4
Saul Dominguez <saul.vau@gmail.com>
2024-03-16 17:37:48 +0100

    no hay mucho avance

--- wallaCoding-ads/ads-controller.js
+++ wallaCoding-ads/ads-controller.js
@@ -7,6 +7,7 @@ const ads = await getAds();
 function renderAds(ads, adsList){
         ads.forEach(ad => {
         const adItem=document.createElement('div');
+        adItem.className='contenedor-ad';
         adItem.innerHTML = buildAd(ad);
         adsList.appendChild(adItem);
     })
